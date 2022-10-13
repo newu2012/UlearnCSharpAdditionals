037a04299888662c40fb699f19bb851d4dd523f5
newu2012 <newu2011@gmail.com>
2022-10-14 00:24:21 +0500

    Add Arrays project

---
+++ Arrays/NotCoveredInLectures.cs
@@ -0,0 +1,93 @@
+﻿namespace Arrays;
+
+public static class NotCoveredInLectures
+{
+    public static void RunAllUncoveredFeatures()
+    {
+        int[] numbers = { 1, 2, 3, 5 };
+
+        // получение элемента массива
+        Console.WriteLine(numbers[3]); // 5
+
+        // получение элемента массива в переменную
+        var n = numbers[1]; // 2
+        Console.WriteLine(n); // 2
+
+        // изменим второй элемент массива
+        numbers[1] = 505;
+
+        Console.WriteLine(numbers[1]); // 505
+        try
+        {
+            Console.WriteLine(numbers[6]); // ! Ошибка - в массиве только 4 элемента
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            // throw; // Расскоментировать, чтобы обратно кинуть ошибку
+        }
+
+        // Длина массива
+        Console.WriteLine(numbers.Length); // 4
+
+        numbers = new[] { 1, 2, 3, 5 };
+        Console.WriteLine(numbers[numbers.Length - 1]); // 5 - первый с конца или последний элемент
+        Console.WriteLine(numbers[numbers.Length - 2]); // 3 - второй с конца или предпоследний элемент
+        Console.WriteLine(numbers[numbers.Length - 3]); // 2 - третий элемент с конца
+
+        // Начиная с C# 8 можно так, но на ulearn шарп старый, так что красиво делать не получится
+        Console.WriteLine(numbers[^1]); // 5 - первый с конца или последний элемент
+        Console.WriteLine(numbers[^2]); // 3 - второй с конца или предпоследний элемент
+        Console.WriteLine(numbers[^3]); // 2 - третий элемент с конца
+
+        
+        // Разворот массива
+        Console.WriteLine();
+        int[] numbersToReverse = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+        numbersToReverse.ToList().ForEach(Console.WriteLine); // Вывод элементов в консоль, так вроде на ulearn нельзя 
+        Console.WriteLine();
+        
+        ReverseArray(numbersToReverse);
+        numbersToReverse.ToList().ForEach(Console.WriteLine);
+        Console.WriteLine();
+        
+        numbersToReverse = ReverseArray(numbersToReverse);
+        numbersToReverse.ToList().ForEach(Console.WriteLine);
+        
+        
+        // Что ещё можно делать с массивами
+        Console.WriteLine();
+        
+        // Удобно записывать в них Диапазон чисел
+        numbers = Enumerable.Range(1, 10).ToArray(); 
+        numbers.ToList().ForEach(Console.WriteLine);
+
+        
+        var numbers2 = new int[numbers.Length];
+        numbers.CopyTo(numbers2, 0); // Копировать массив по значениям
+        
+        Console.WriteLine(numbers == numbers2);
+        Console.WriteLine(numbers.SequenceEqual(numbers2)); // Сравнение по значению элементов
+        
+        numbers2[0] = 100;
+        Console.WriteLine(numbers == numbers2);
+        Console.WriteLine(numbers.SequenceEqual(numbers2));
+        Console.WriteLine($"numbers: {numbers[0]} - numbers2: {numbers2[0]}");
+    }
+
+    private static int[] ReverseArray(int[] array) // Что есть лишнего в методе?
+    {
+        var n = array.Length; // длина массива
+        var k = n / 2; // середина массива
+        int temp; // вспомогательный элемент для обмена значениями
+        
+        for (var i = 0; i < k; i++)
+        {
+            temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+        }
+
+        return array;
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Arrays;
+
+public class Program
+{
+    static int staticGlobal = 1;
+    int globalValueVariable = 123;
+    string globalReferenceVariable = "123";
+
+    public static void Main()
+    {
+        // Что такое ссылочные типы и типы значения. Какие типы к чему относятся.
+        //  Типы значений (Value types)
+        var simpleNumber = 1;
+        var numberWithFloatPoint = 1.0;
+        var boolean = false;
+        var character = 'A';
+
+        //  Ссылочные типы (Reference types)
+        var strEmpty1 = "";
+        var strEmpty2 = string.Empty;
+        var array = new int[5];
+        var list = new List<int>();
+
+        
+        //  Что такое стек и куча. Что происходит при вызове метода. Что происходит, если из метода вызвать самого себя. Почему стек ограничен.
+        IncrementTo(simpleNumber, 100);
+
+        
+        //  Как хранятся в памяти типы значения. Их значения по умолчанию.
+        int someInt;
+        double someDouble;
+        bool someBool;
+        char someChar;
+
+        
+        //  Как хранятся в куче ссылочные типы. Как хранятся массивы ссылочных типов и массивы типов значений.
+        //  (Удобно рисовать таблицу сравнения типов, подобно той, что была в видеолекциях.)
+        int[] intArray;
+        var intArray1 = new int[5];
+        var intArray2 = new[] { 1, 2, 3, 4, 5 };
+
+        int[] strArray;
+        var strArray1 = new string[5];
+        var strArray2 = new[] { "1", "2", "3", "4", "5" };
+
+        
+        //  Как хранится в памяти ссылка, что такое null.
+        //  Как хранятся пустая строка и пустой массив.
+        //  (Ссылка по сути тип значения число — адрес в памяти области в куче.)
+        string str;
+        string str1 = "123";
+        string str2 = "123";
+        str = str1;
+        var condStr1 = str == str1;
+        var condStr2 = str1 == str2;
+
+        int[] arr;
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        arr = arr1;
+        var condArr1 = arr == arr1;
+        var condArr2 = arr == arr2;
+
+        
+        //  Где хранятся локальные переменные и где «глобальные» статические.
+        var localValueVariable = 123;
+        var localReferenceVariable = "123";
+
+
+        // Про сборщик мусора. Он работает, когда захочет.
+        // Я хз честно что и как рассказать чтобы было интересно и понятно...
+        
+        
+        //  Дополнительное
+        NotCoveredInLectures.RunAllUncoveredFeatures();
+    }
+
+    private static void IncrementTo(int numberToIncrement, int expectedNumber)
+    {
+        numberToIncrement++; // Comment this to fall in Stack Overflow
+        Console.WriteLine($"Number is {numberToIncrement}");
+        if (numberToIncrement < expectedNumber)
+            IncrementTo(numberToIncrement, expectedNumber);
+    }
+}
